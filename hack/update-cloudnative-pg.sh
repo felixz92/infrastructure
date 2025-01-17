33d7b61c304726d398e78544b8b20619bba36cf5
Felix Zippel <contact@zippelf.com>
2025-01-17 22:31:24 +0100

    Add image catalog

--- hack/update-cloudnative-pg.sh
+++ hack/update-cloudnative-pg.sh
@@ -6,11 +6,13 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 VERSION="1.25.0"
 
 url=https://github.com/cloudnative-pg/cloudnative-pg/releases/download/v${VERSION}/cnpg-${VERSION}.yaml
+image_catalog=https://raw.githubusercontent.com/cloudnative-pg/postgres-containers/main/Debian/ClusterImageCatalog-bullseye.yaml
 
 CRD_OUTPUT_DIR="${REPO_ROOT}/system/crds/cloudnative-pg"
 OUTPUT_DIR="${REPO_ROOT}/system/controllers/cloudnative-pg/base"
 
 temp=$(mktemp -d)
+curl -sL "${image_catalog}" -o "${OUTPUT_DIR}/image-catalog.yaml"
 
 curl -sL "${url}" -o "${temp}/cnpg.yaml"
 (cd "${temp}" && yq -s '.kind +"-" + .metadata.name | downcase + ".yaml"' cnpg.yaml)
