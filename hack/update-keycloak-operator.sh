d70679f7a12e43013ea6ccec8d4d94ec03232710
Felix Zippel <contact@zippelf.com>
2025-01-25 16:53:10 +0100

    Delete keycloak operator

--- hack/update-keycloak-operator.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-REPO_ROOT="$(git rev-parse --show-toplevel)"
-
-VERSION="26.1.0"
-OUTPUT_DIR="${REPO_ROOT}/system/controllers/keycloak-operator/base"
-
-curl -s -L --fail "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${VERSION}/kubernetes/kubernetes.yml" -o "${OUTPUT_DIR}/operator.yaml"
