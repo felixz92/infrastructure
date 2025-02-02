40f7685609b191ffe28fe5cfadb91d7e07d5cd45
Felix Zippel <contact@zippelf.com>
2025-02-02 02:23:06 +0100

    Deploy pgadmin

--- infrastructure/pgadmin/base/config_local.py
+++ infrastructure/pgadmin/base/config_local.py
@@ -11,7 +11,7 @@ OAUTH2_CONFIG = [{
     'OAUTH2_AUTHORIZATION_URL': 'https://authentik.${BASE_DOMAIN}/application/o/authorize/',
     'OAUTH2_API_BASE_URL': 'https://authentik.${BASE_DOMAIN}/',
     'OAUTH2_USERINFO_ENDPOINT': 'https://authentik.${BASE_DOMAIN}/application/o/userinfo/',
-    'OAUTH2_SERVER_METADATA_URL': 'https://authentik.${BASE_DOMAIN}/application/o/<App Slug>/.well-known/openid-configuration',
+    'OAUTH2_SERVER_METADATA_URL': 'https://authentik.${BASE_DOMAIN}/application/o/pgadmin-rzzr7-application/.well-known/openid-configuration',
     'OAUTH2_SCOPE': 'openid email profile',
     'OAUTH2_ICON': 'fa-openid',
 }]
